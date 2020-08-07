fbbd162d41fe846fc988f86db3965236220a8599
Cobraaz <anujbansal077@gmail.com>
2020-08-07 14:55:32 +0530

    Added projects data to populate

--- populate/data.js
+++ populate/data.js
@@ -60,6 +60,69 @@ const data = {
       status: "published",
     },
   ],
+  projects: [
+    {
+      header: "MERN",
+      img: "https://i.ibb.co/fxLVF2B/Devconnector-Homescreen.png",
+      title: "DevConnector",
+      description:
+        "This app is for developers to add their profile with experience and education, post , comments. This app is designed with MERN stack",
+      github: "https://github.com/Cobraaz/Developer-connector-Reactjs-Nodejs",
+      link: "https://devs-connectors.herokuapp.com/",
+    },
+    {
+      header: "MERN",
+      img: "https://i.ibb.co/QX1Vjj2/DAshboard.png",
+      title: "Instagram Clone",
+      description:
+        "Social network for Users to make profile, share post, likes post, comment on post and many more stuff.",
+      github: "https://github.com/Cobraaz/instagram-clone",
+      link: "https://instagram-clone007.herokuapp.com/",
+    },
+    {
+      header: "Reactjs & firebase",
+      img:
+        "https://res.cloudinary.com/cobraaz/image/upload/v1596526366/Screenshot_11_cpsmes.png",
+      title: "CrwnClothing",
+      description: "crwn clothing using React & firebase",
+      github: "https://github.com/Cobraaz/crwn-clothing",
+      link: "https://crwnn-clothing.herokuapp.com/",
+    },
+    {
+      header: "Reactjs",
+      img: "https://i.ibb.co/fxLVF2B/Devconnector-Homescreen.png",
+      title: "Corona Tracker",
+      description: "This is small project on corona tracker",
+      github: "https://github.com/Cobraaz/corona-tracker",
+      link: "https://corona-trackerr.herokuapp.com/",
+    },
+    {
+      header: "MERN",
+      img:
+        "https://res.cloudinary.com/cobraaz/image/upload/v1596526366/Screenshot_12_u2hgak.png",
+      title: "Smart Brain",
+      description: "This app is used to recognize faces in the picture",
+      github: "https://github.com/Cobraaz/smart-brain",
+      link: "https://smart-brain-facerrecognition.herokuapp.com/",
+    },
+    {
+      header: "React.js & Node.js",
+      img:
+        "https://res.cloudinary.com/cobraaz/image/upload/v1596526364/Screenshot_13_r5zhly.png",
+      title: "Realtime Chat Application",
+      description: "This project uses socket.io, reactjs, node",
+      github: "https://github.com/Cobraaz/chat-application",
+    },
+    {
+      header: "Python & Django",
+      img:
+        "https://res.cloudinary.com/cobraaz/image/upload/v1596526364/Screenshot_14_x4brce.png",
+      title: "Password Checker",
+      description: "To check how many times your password is Hacked",
+      github: "https://github.com/Cobraaz/pwned_password_Django",
+      link: "https://murmuring-tundra-59094.herokuapp.com/",
+    },
+  ],
 };
 
 module.exports = data;
